3e16e3c70ddce98135f4cb0ffcb79fe66704fc95
Vetrivel <pgvetrivel@gmail.com>
2024-08-30 14:38:36 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,17 +5,6 @@ from setuptools import setup, find_packages
 setup(
     name='returns_calculator',
     version='1.0.0',
-    description='A package for calculating financial returns.',
-    author='Vetrivel Gomathinayagan',
-    author_email='pgvetrivel@gmail.com',
-    packages=["returnscalculator"],
-    install_requires=[
-        'pandas>=1.0.0',
-        'numpy>=1.18.0'
-    ],
-    classifiers=[
-        'Programming Language :: Python :: 3',        
-        'Operating System :: OS Independent',
-    ],
+    packages=["returnscalculator"],   
     python_requires='>=3.6',
 )
